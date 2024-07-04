e1e493faf077bf3ec141c0cd0f638a454c7ca4af
Cremecho <916783659@qq.com>
2024-07-04 20:06:15 +0800

    implement kd-knn

--- models/Knn_kd.py
+++ models/Knn_kd.py
@@ -1,10 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
-import matplotlib.pyplot as plt
-import numpy as np
 import sys
-
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import time
+
+
 import random
 import torch
 import os
@@ -61,119 +63,120 @@ class BCPDataset(object):
         self.x = inputs
         self.y = label
 
-# As Knn isn't Unsupervised Model, the calling line should be modified to:
-# output = self.model(x, y)
-class Knn(object):
-    def __init__(self,xy, K):
+
+class Node(object):
+    def __init__(self, value, label, left=None, right=None):
+        self.val = value
+        self.label = label
+        self.left = left
+        self.right = right
+
+
+
+class KDTree(object):
+    def __init__(self, K, K_knn):
         super().__init__()
         self.l1 = nn.Linear(2,2)
-        self.xy = xy
         self.K = K
-        self.k_near_dis = np.ones((K)) * 999
-        self.k_near_point = np.zeros((K, 2))
-        self.k_near_label = np.zeros((K))
+        self.K_knn = K_knn
+        self.tree = None
+        self.k_near_dis = np.ones((K_knn)) * 999
+        self.k_near_point = np.zeros((K_knn, K))
+        self.k_near_label = np.zeros((K_knn))
 
-    def euclid_distance(self, p1, p2):
-        d = sum((p1 - p2).pow(2))
-        return d
+    # 建树
+    def build_tree(self, x, xy, depth):
+        l = len(x)
+        if l == 0:
+            return None
+        split = depth % self.K  # 当前分割维度
+        sorted_data = sorted(x, key=lambda x: x[split])  # 按照某个维度数据进行排序
+        sorted_label = sorted(xy, key=lambda x: x[split])
 
-    def forward(self, x, y):
-        # x.shape [400,2]
-        n, features = x.shape
-        k_distances = 999 * torch.ones((n, self.K))
-        k_labels = torch.zeros((n, self.K))
-        output = torch.zeros((n,))
-        for i in range(n):
-            for j in range(n):
-                if i == j:
-                    continue
-                point1 = x[i, :]
-                point2 = x[j, :]
-                distance = self.euclid_distance(point1, point2)
-                if distance < k_distances[i, -1]:
-                    k_distances[i, -1] = distance
-                    k_labels[i, -1] = y[j]
-                    st = k_distances[i, :].sort()
-                    k_distances[i, :] = st.values
-                    k_labels[i, :] = k_labels[i, st.indices]
-        mask = k_labels.sum(dim=1) > (self.K) / 2
-        output[mask] = 1
+        mid_idx = l // 2
+        left_data = sorted_data[:mid_idx]
+        right_data = sorted_data[mid_idx+1:]  # mid_idx已经作为当前层根节点了，所以不能加mid_idx这个数据对
+        left_label = sorted_label[:mid_idx]
+        right_label = sorted_label[mid_idx+1:]
 
-        x, y = x.cpu().detach().numpy(), y.cpu().detach().numpy()
-        output = output.numpy()
-        acc = np.sum(output == y) / len(y)
-        print("Acc: %.3f" % acc)
+        cur_node = Node(sorted_data[mid_idx], sorted_label[mid_idx][-1])  # 将中位数作为当前根节点
+        cur_node.left = self.build_tree(left_data, left_label, depth + 1)  # 递归构建当前根节点左子树
+        cur_node.right = self.build_tree(right_data, right_label, depth + 1)  # 递归构建当前根节点右子树
 
-        # org plot
-        plt.figure(1)
-        for i in range(len(y)):
-            if y[i] == 0:
-                plt.scatter(x[i,0], x[i,1], c='r', marker="*")
-            else:
-                plt.scatter(x[i, 0], x[i, 1],c='b', marker="^")
-        plt.show()
-        plt.close()
+        self.tree = cur_node
+        return cur_node
 
-        plt.figure(2)
-        for i in range(len(y)):
-            if output[i] == 0:
-                plt.scatter(x[i,0], x[i,1], c='r', marker="*")
-            else:
-                plt.scatter(x[i, 0], x[i, 1],c='b', marker="^")
-        plt.show()
-        plt.close()
-
-        sys.exit()
-
-        output.cuda()
-        return output
-
-    def euclid_distance_np(self, point1, point2):
+    # 计算欧氏距离
+    def euclid_distance(self, point1, point2):
         return np.linalg.norm(np.array(point1) - np.array(point2))
 
-    def predict(self, target):
-        for i in range(len(self.xy)):
-            cur_point = self.xy[i][:2]
-            cur_label = self.xy[i][-1]
-            dis = self.euclid_distance_np(target, cur_point)
+    # 最近邻搜索（1个target）
+    def search_nn(self, tree, target):
+        def dfs(node, depth):
+            if not node:
+                return
 
-            if dis < self.k_near_dis[-1]:
+            # 第一步要先找到叶子节点，之后再进行回溯
+            split = depth % self.K
+            if target[split] < node.val[split]:
+                dfs(node.left, depth + 1)  # 该行包括下面的dfs行，第一个参数输入都是某个节点node.left或node.right，所以本质上该节点node并没有改变，每次改变的都是函数输入参数，所以当dfs递归结束并回退到该位置时，该节点没有变化，以此实现回溯上一个节点的过程！
+            else:
+                dfs(node.right, depth + 1)
+            # ======= 到这结束就已经找到了某个路径下最终叶子节点 =======
+
+            # 开始回溯，以当前叶子节点和目标节点的距离作为初始的最小距离
+            dis = self.euclid_distance(node.val, target)
+            if not node.val in self.k_near_point and dis < self.k_near_dis[-1]:
                 self.k_near_dis[-1] = dis
-                self.k_near_point[-1] = cur_point
-                self.k_near_label[-1] = cur_label
+                self.k_near_label[-1] = node.label
+                self.k_near_point[-1,:] = node.val
 
                 idx = np.argsort(self.k_near_dis)
                 self.k_near_dis = self.k_near_dis[idx]
                 self.k_near_label = self.k_near_label[idx]
                 self.k_near_point = self.k_near_point[idx]
+                # label
 
+            # 判断是否遍历该节点另一边子树
+            if abs(node.val[split] - target[split]) < self.k_near_dis[-1]:
+                if target[split] < node.val[split]:  # 第一次从上往下遍历时目标节点值小于当前节点值时，就遍历左边；回溯时因为要遍历另一边，所以刚好相反！
+                    dfs(node.right, depth + 1)
+                else:
+                    dfs(node.left, depth + 1)
+
+        dfs(tree, 0)
         return1, return2, return3 = self.k_near_point, self.k_near_dis, self.k_near_label
-        self.k_near_dis = np.ones((self.K)) * 999
-        self.k_near_point = np.zeros((self.K, 2))
-        self.k_near_label = np.zeros((self.K))
+        self.k_near_dis = np.ones((self.K_knn)) * 999
+        self.k_near_point = np.zeros((self.K_knn, self.K))
+        self.k_near_label = np.zeros((self.K_knn))
 
         return return1, return2, return3
 
 
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    # dataset, target, K = [[2, 3], [5, 4], [9, 6], [4, 7], [8, 1], [7, 2]], [1, 5], 2
     dataset_train = BCPDataset('train')
     dataset_test = BCPDataset('test')
 
-    train_xy = [[a[0], a[1], b] for a, b in zip(dataset_train.x, dataset_train.y)]
-    K = 3
-    knn = Knn(train_xy, K=K)
+    K = 2
+    K_knn = 3
+
+    tree = KDTree(K, K_knn)
+    train_xy = [[a[0], a[1], b] for a,b in zip(dataset_train.x, dataset_train.y)]
+    tree.build_tree(dataset_train.x, train_xy, 0)
 
     added_labels = set()
     confusion_matrix = [0,0,0,0]
     plt.figure(1)
-    total_t = 0.
+    total_t = 0
     for target, label in zip(dataset_test.x, dataset_test.y):
         t1 = time.time()
-        nearest_point, nearest_dis, nearest_label = knn.predict(target)
+        nearest_point, nearest_dis, nearest_label = tree.search_nn(tree.tree, target)
         delta_t = time.time() - t1
         total_t += delta_t
-        pred_label = 1 if sum(nearest_label) > K / 2 else 0
+        pred_label = 1 if sum(nearest_label) > K_knn / 2 else 0
 
         if label == 1 and pred_label == 1:
             confusion_matrix[0] = confusion_matrix[0] + 1
